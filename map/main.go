7c11e7372a653e2c453f1102137f02059deac02d
Shikhar Dubey <shikhar.db@gmail.com>
2019-01-24 06:10:09 +0530

    Make map

---
+++ map/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var colors := map[string]string
+
+	colors := make(map[string]string)
+
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	fmt.Println(colors)
+}

e7ed8f4433fcb118fe9683fa6ba83f4db7fbecbc
Shikhar Dubey <shikhar.db@gmail.com>
2019-01-24 05:33:30 +0530

    Struct functions

--- structs/main.go
+++ structs/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type contactInfo struct {
 	email   string
 	zipcode int
@@ -8,16 +10,27 @@ type contactInfo struct {
 type person struct {
 	firstName string
 	lastName  string
-	contact   contactInfo
+	contactInfo
 }
 
 func main() {
 	jim := person{
 		firstName: "Jim",
-		lastName: "Party",
-		contact: contactInfo{
-			email: "jim@gmail.com",
-			zipcode: 94000
-		}
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipcode: 94000,
+		},
 	}
+
+	jim.updateName("jimmy")
+	jim.print()
+}
+
+func (p person) updateName(newFirstName string) {
+	p.firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
